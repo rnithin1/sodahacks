d3234f9e329ad1d70f869af1af6a52adad1f364f
David Ben-Gurion <rnithin@berkeley.edu>
2018-04-15 09:41:42 -0700

    fixed hour and kmeans

--- hour.py
+++ hour.py
@@ -11,7 +11,10 @@ pickup_datetime = df['pickup_datetime'].unique().tolist()
 df_by_hour = [df.loc[df['pickup_datetime'] == i] for i in range(0, 24, 2)]
 i = 0
 for hour in df_by_hour:
-    hour = hour[hour.pickup_latitude > 39 and hour.pickup_longitude < -72]
+    #hour = hour[hour.pickup_latitude > 39 and hour.pickup_longitude < -72]
+    print(hour.columns[[0]])
+    hour.drop(hour.columns[[0]], axis=1, inplace=True)
+    hour = hour[(hour != 0).all(1)]
     hour.to_csv("df_concat_{}.csv".format(i))
     i += 2
 
--- kmeans.py
+++ kmeans.py
@@ -4,15 +4,14 @@ from scipy.cluster.vq import *
 
 print("Starting")
 df_by_hour = []
-for i in range(0, 24, 2):
+for i in range(0, 24, 4):
     df = pd.read_csv("df_concat_{}.csv".format(i))
-    df.drop(df.columns[[0, 1]], axis=1, inplace=True)
     df_by_hour.append(df)
 print("Starting kmeans")
 intersections_by_hour = []
 for df in df_by_hour:
     print("Beginning")
     #df = whiten(df)
-    intersections_by_hour.append(kmeans2(df.values, 12460))
+    intersections_by_hour.append(kmeans2(df.values, 12460, iter=6, minit='points'))
 for i in range(len(intersections_by_hour)):
-    np.save('centroids_{}.npy'.format(i * 2), intersections_by_hour[i])
+    np.save('centroids_{}.npy'.format(i * 4), intersections_by_hour[i])
