62d6a21058e3b83bf555ce971204dd08ba5ccbce
David Ben-Gurion <rnithin@berkeley.edu>
2018-04-15 07:54:56 -0700

    fixed kmeans function

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,18 @@
 import numpy as np
+import pandas as pd
 from scipy.cluster.vq import *
 
-totals = np.load("vstackedPoints.dat.npy")
-intersections = kmeans2(totals, 12000)
-np.save('centroids.npy', intersections)
+print("Starting")
+df_by_hour = []
+for i in range(0, 24, 2):
+    df = pd.read_csv("df_concat_{}.csv".format(i))
+    df.drop(df.columns[[0, 1]], axis=1, inplace=True)
+    df_by_hour.append(df)
+print("Starting kmeans")
+intersections_by_hour = []
+for df in df_by_hour:
+    print("Beginning")
+    #df = whiten(df)
+    intersections_by_hour.append(kmeans2(df.values, 12460))
+for i in range(len(intersections_by_hour)):
+    np.save('centroids_{}.npy'.format(i * 2), intersections_by_hour[i])
